22d950360d2faa83bec005e6e0503ae2b989d247
cludley <cludley@anamosa.k12.ia.us>
2025-10-27 18:24:46 +0000

    reverted code

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var distanceInMiles = 100;
 
+function square(number) {
+  return number * number;
+}
+
 function estimatedArrivalTime {
- 
+
   var milesPerHour = 60; 
 
   return distanceInMiles / milesPerHour;
 }
 
-document.getElementById('result').innerHTML = estimatedArrivalTime();
+document.getElementById('result').innerHTML = estimatedArrivalTime;
