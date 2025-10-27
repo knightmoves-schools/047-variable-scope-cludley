ddd423203713651f52134b9396defbee5100b2e7
cludley <cludley@anamosa.k12.ia.us>
2025-10-27 18:26:06 +0000

    b

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function square(number) {
   return number * number;
 }
 
-function estimatedArrivalTime {
+function estimatedArrivalTime() {
 
   var milesPerHour = 60; 
 
   return distanceInMiles / milesPerHour;
 }
 
-document.getElementById('result').innerHTML = estimatedArrivalTime;
+document.getElementById('result').innerHTML = estimatedArrivalTime();
